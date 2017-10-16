769e4b9e5e2433fe275da3cfaa7359ecc0bebba7
Malgorzata Haniszewska and Qianqian Hu <malgorzata@>
2017-10-16 13:44:53 -0700

    add functions to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,38 @@
+var saySomething = function(something) {
+  alert(something);
+};
+
 var add = function(number1, number2) {
   return number1 + number2;
+};
+var substract = function(number1, number2) {
+  return number1 - number2
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+
+var addResult = add(number1, number2);
+var addDisplay = "The result of adding is " + addResult;
+
+var substractResult = substract(number1, number2);
+var substractDisplay = "The result of substracting is " + substractResult;
+
+var multiplyResult = multiply(number1, number2);
+var multiplyDisplay = "The result of multiplying is " + multiplyResult;
+
+var divideResult = divide(number1, number2);
+var divideDisplay = "The result of dividing is " + divideResult;
+
+alert(addDisplay);
+alert(substractDisplay);
+alert(multiplyDisplay);
+alert(divideDisplay);
